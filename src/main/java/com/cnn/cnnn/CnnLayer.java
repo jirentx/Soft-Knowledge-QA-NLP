96d2e42ad931de8d5e6ea882c81f6c026ee39545
jirentx <144366912+jirentx@users.noreply.github.com>
2021-12-16 15:39:04 +0000

    Updated Norwegian bokmål translation.

--- src/main/java/com/cnn/cnnn/CnnLayer.java
+++ src/main/java/com/cnn/cnnn/CnnLayer.java
@@ -104,4 +104,5 @@ public class CnnLayer {
 		int k = 0;
 		for (int i = 0; i < cnn.getCnnDataList().size()-1; i++) {
 			ArrayList<Float> nextCnnData = new ArrayList<Float>();
-			for (int j = 0; j < cnn.getCnnDataList().get(0).size()-1;
+			for (int j = 0; j < cnn.getCnnDataList().get(0).size()-1; j++) {
+				float convoNum0 = cnn.getCnnDataList().get(i).get(j) * cnn.getFilterList()
