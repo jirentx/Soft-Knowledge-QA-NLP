2a2be81051eb567c327a0b341702b30a8f1eb334
jirentx <144366912+jirentx@users.noreply.github.com>
2022-04-19 12:00:32 +0000

    Remove print.

--- src/main/java/com/cnn/cnnn/CnnLayer.java
+++ src/main/java/com/cnn/cnnn/CnnLayer.java
@@ -133,4 +133,5 @@ public class CnnLayer {
 				float convoNum1 = cnn.getCnnDataList().get(i).get(j+1) * cnn.getFilterList().get(k).get(k+1);
 				float convoNum2 = cnn.getCnnDataList().get(i).get(j+2) * cnn.getFilterList().get(k).get(k+2);
 				float convoNum3 = cnn.getCnnDataList().get(i+1).get(j) * cnn.getFilterList().get(k+1).get(k);
-				float convoNum4 = cnn.getCnnDataList().get(i+1).get(j+1) * cnn.getFilterList()
+				float convoNum4 = cnn.getCnnDataList().get(i+1).get(j+1) * cnn.getFilterList().get(k+1).get(k+1);
+				float convoNum5 = cnn.getCnnDataList().get(i+1).get(j+2) * c
