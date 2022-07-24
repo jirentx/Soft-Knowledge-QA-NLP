7a5eb8d7b3bae45b80b03028d5edee7c786da6c3
jirentx <144366912+jirentx@users.noreply.github.com>
2022-07-24 20:21:38 +0000

    styles added

--- src/main/java/com/cnn/cnnn/ModelTrain.java
+++ src/main/java/com/cnn/cnnn/ModelTrain.java
@@ -4,4 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-imp
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashM
