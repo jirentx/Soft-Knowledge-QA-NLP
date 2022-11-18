b7ace4313f9b98383126ee5b72200fecd5e25df5
jirentx <144366912+jirentx@users.noreply.github.com>
2022-11-18 11:37:32 +0000

    added project description

--- src/main/java/com/cnn/cnnn/RELU.java
+++ src/main/java/com/cnn/cnnn/RELU.java
@@ -4,4 +4,9 @@ import com.cnn.entity.Cnn;
 
 /**
  * @author 朱宏
- * @description：RELU激活函数，去
+ * @description：RELU激活函数，去除不必要的特征
+ * 2019年4月3日
+ */
+public class RELU {
+	public static void  reluFunc(Cnn cnn) {
+		int co
