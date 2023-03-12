f9c0b3196f0a4e2f76b8f5a3dfdb748738b47e41
jirentx <144366912+jirentx@users.noreply.github.com>
2023-03-12 18:46:01 +0000

    removed unused folder

--- src/main/java/com/word2vec/Word2VEC.java
+++ src/main/java/com/word2vec/Word2VEC.java
@@ -9,4 +9,8 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.H
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import ja
