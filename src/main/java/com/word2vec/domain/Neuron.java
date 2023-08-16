ce52b4b8df9c0aa009ce5231ad3d94a1b409f990
jirentx <144366912+jirentx@users.noreply.github.com>
2023-08-16 13:27:21 +0000

    PR suggestions

--- src/main/java/com/word2vec/domain/Neuron.java
+++ src/main/java/com/word2vec/domain/Neuron.java
@@ -2,4 +2,9 @@ package com.word2vec.domain;
 
 public abstract class Neuron implements Comparable<Neuron> {
   public double freq;
-  pu
+  public Neuron parent;
+  public int code;
+  // 语料预分类
+  public int category = -1;
+
+  public int
