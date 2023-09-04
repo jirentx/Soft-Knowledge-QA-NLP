69c55869961ca4b6d52f0b99946626fdc2120b29
jirentx <144366912+jirentx@users.noreply.github.com>
2023-09-04 17:22:50 +0000

    merged to master

--- src/main/java/com/word2vec/domain/WordNeuron.java
+++ src/main/java/com/word2vec/domain/WordNeuron.java
@@ -34,4 +34,7 @@ public class WordNeuron extends Neuron {
   public WordNeuron(String name, double freq, int layerSize) {
     this.name = name;
     this.freq = freq;
-    this.syn0 = new double[lay
+    this.syn0 = new double[layerSize];
+    Random random = new Random();
+    for (int i = 0; i < syn0.length; i++) {
+      syn0[i] 
