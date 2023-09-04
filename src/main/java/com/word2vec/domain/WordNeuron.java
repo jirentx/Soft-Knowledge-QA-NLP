635f4cf5aa31f875958a06d26040a002144f0523
jirentx <144366912+jirentx@users.noreply.github.com>
2023-09-04 13:44:43 +0000

    Update ChangeLog.txt

--- src/main/java/com/word2vec/domain/WordNeuron.java
+++ src/main/java/com/word2vec/domain/WordNeuron.java
@@ -31,4 +31,7 @@ public class WordNeuron extends Neuron {
     return neurons;
   }
 
-  public WordNeuron(String name, double freq, int
+  public WordNeuron(String name, double freq, int layerSize) {
+    this.name = name;
+    this.freq = freq;
+    this.syn0 = new double[lay
