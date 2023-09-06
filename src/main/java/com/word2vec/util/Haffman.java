286ac03ac1580f56498caaebe3097f45efa14df5
jirentx <144366912+jirentx@users.noreply.github.com>
2023-09-06 20:07:54 +0000

    Tiny fixes.

--- src/main/java/com/word2vec/util/Haffman.java
+++ src/main/java/com/word2vec/util/Haffman.java
@@ -1,4 +1,10 @@
 package com.word2vec.util;
 
 import java.util.Collection;
-import java.util
+import java.util.TreeSet;
+
+import com.word2vec.domain.HiddenNeuron;
+import com.word2vec.domain.Neuron;
+
+/**
+ * 构
