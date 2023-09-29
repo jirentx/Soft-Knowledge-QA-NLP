d66170f185b99794e702a92c1ed36376cda07b28
jirentx <144366912+jirentx@users.noreply.github.com>
2023-09-29 18:05:00 +0000

    Fixed a build error.

--- src/main/java/com/word2vec/util/WordKmeans.java
+++ src/main/java/com/word2vec/util/WordKmeans.java
@@ -12,3 +12,103 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import com.word2vec.Word2VEC;
+
+/**
+ * keanmeans聚类
+ * 
+ * @author ansj
+ * 
+ */
+public class WordKmeans {
+
+    public static void main(String[] args) throws IOException {
+        Word2VEC vec = new Word2VEC();
+        vec.loadGoogleModel("vectors.bin");
+        System.out.println("load model ok!");
+        WordKmeans wordKmeans = new WordKmeans(vec.getWordMap(), 50, 50);
+        Classes[] explain = wordKmeans.explain();
+
+        for (int i = 0; i < explain.length; i++) {
+            System.out.println("--------" + i + "---------");
+            System.out.println(explain[i].getTop(10));
+        }
+
+    }
+
+    private HashMap<String, float[]> wordMap = null;
+
+    private int iter;
+
+    private Classes[] cArray = null;
+
+    public WordKmeans(HashMap<String, float[]> wordMap, int clcn, int iter) {
+        this.wordMap = wordMap;
+        this.iter = iter;
+        cArray = new Classes[clcn];
+    }
+
+    public Classes[] explain() {
+        //first 取前clcn个点
+        Iterator<Entry<String, float[]>> iterator = wordMap.entrySet().iterator();
+        for (int i = 0; i < cArray.length; i++) {
+            Entry<String, float[]> next = iterator.next();
+            cArray[i] = new Classes(i, next.getValue());
+        }
+
+        for (int i = 0; i < iter; i++) {
+            for (Classes classes : cArray) {
+                classes.clean();
+            }
+
+            iterator = wordMap.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Entry<String, float[]> next = iterator.next();
+                double miniScore = Double.MAX_VALUE;
+                double tempScore;
+                int classesId = 0;
+                for (Classes classes : cArray) {
+                    tempScore = classes.distance(next.getValue());
+                    if (miniScore > tempScore) {
+                        miniScore = tempScore;
+                        classesId = classes.id;
+                    }
+                }
+                cArray[classesId].putValue(next.getKey(), miniScore);
+            }
+
+            for (Classes classes : cArray) {
+                classes.updateCenter(wordMap);
+            }
+            System.out.println("iter " + i + " ok!");
+        }
+
+        return cArray;
+    }
+
+    public static class Classes {
+        private int id;
+
+        private float[] center;
+
+        public Classes(int id, float[] center) {
+            this.id = id;
+            this.center = center.clone();
+        }
+
+        Map<String, Double> values = new HashMap<>();
+
+        public double distance(float[] value) {
+            double sum = 0;
+            for (int i = 0; i < value.length; i++) {
+                sum += (center[i] - value[i])*(center[i] - value[i]) ;
+            }
+            return sum ;
+        }
+
+        public void putValue(String word, double score) {
+            values.put(word, score);
+        }
+
+        /**
+         * 重新计算中心点
+         * @param wordMap
