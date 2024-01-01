115b60a801c60a5f166c80a585ad55b04c9f03b0
jirentx <144366912+jirentx@users.noreply.github.com>
2024-01-01 08:44:41 +0000

    remove debug console.log

--- target/tmp/jsp/org/apache/jsp/index_jsp.java
+++ target/tmp/jsp/org/apache/jsp/index_jsp.java
@@ -13,4 +13,7 @@ import javax.servlet.http.*;
 import javax.servlet.jsp.*;
 
 public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
-    implements org.apache.jasper.
+    implements org.apache.jasper.runtime.JspSourceDependent,
+                 org.apache.jasper.runtime.JspSourceImports {
+
+  private static
